dc2ead2e046faea5796600d18e6d8cec50d9e44b
Eraj Mirzoev <mirzoeva145@gmail.com>
2025-07-15 17:07:00 +0500

    explore get

--- src/app/explore/page.jsx
+++ src/app/explore/page.jsx
@@ -1,8 +1,28 @@
 'use client'
 
+import { video } from "@/assets/icon/layout/svg"
+import { useExplorePage } from "@/store/pages/explore/explore"
+import { API } from "@/utils/config"
+import { useEffect } from "react"
+
 
 export default function Explore(){
+	
+	const { data, getExplore } = useExplorePage()
+	console.log(data)
+
+
+	useEffect((el)=> {
+		getExplore()
+	}, [])
+
 	return <>
-		<h1>Page explodwre</h1>
+		<h1>{data.map((el)=> {
+			return (
+				<div>
+					<video controls src={`${API}/images/${el.images}`}></video>
+				</div>
+			)
+		})}</h1>
 	</>
 }
