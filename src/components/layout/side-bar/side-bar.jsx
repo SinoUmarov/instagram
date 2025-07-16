ce4637c891a105f1775037b1511d95b27a948ff2
Kastro <boboevdjasur@gmail.com>
2025-07-16 13:36:51 +0600

    add swipper in profile , add user avatar in sidebar and bottom bar,fix request

--- src/components/layout/side-bar/side-bar.jsx
+++ src/components/layout/side-bar/side-bar.jsx
@@ -1,11 +1,11 @@
-"use client"
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
-import Profile from '@/assets/icon/layout/instagramDefaultProfile.jpg';
-import { Menu, MenuItem } from '@mui/material';
-import { usePathname } from 'next/navigation';
-import Image from 'next/image';
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import Profile from "@/assets/icon/layout/instagramDefaultProfile.jpg";
+import { Menu, MenuItem } from "@mui/material";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
 import {
   homeIcon,
   homeIconActive,
@@ -24,25 +24,25 @@ import {
   savedIcon,
   problemIcon,
   threads,
-} from '@/assets/icon/layout/svg';
-import { useProfileStore } from '@/store/pages/profile/profile/store-profile';
-import { API } from '@/utils/config';
-
+} from "@/assets/icon/layout/svg";
+import { useProfileStore } from "@/store/pages/profile/profile/store-profile";
+import { API } from "@/utils/config";
+import { jwtDecode } from "jwt-decode";
 
 const NavLink = ({ href, icon, activeIcon, label, isActive }) => (
   <Link
     href={href}
-    className={`flex items-center gap-4 w-[90%] m-auto rounded-md h-[52px] px-4 hover:bg-gray-100 ${isActive(href)}`}
+    className={`flex items-center gap-4 w-[90%] m-auto rounded-md h-[52px] px-4 hover:bg-gray-100 ${isActive(
+      href
+    )}`}
   >
     {isActive(href) ? activeIcon : icon}
     <p className="text-lg">{label}</p>
   </Link>
 );
 
-
 export default function SideBar({ children }) {
-
-  const {getInfo,info} = useProfileStore()
+  const { getInfo, info } = useProfileStore();
   const pathname = usePathname();
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
@@ -56,15 +56,12 @@ export default function SideBar({ children }) {
     setAnchorEl(null);
   };
 
+  let decode = jwtDecode(localStorage.getItem("access_token"));
   useEffect(() => {
-    getInfo()
-    
-    
+    getInfo(decode.sid);
   }, []);
-  console.log('dddd',info);
-  
 
-  const isActive = (path) => pathname === path ? 'font-bold' : 'font-normal';
+  const isActive = (path) => (pathname === path ? "font-bold" : "font-normal");
 
   return (
     <div>
@@ -75,51 +72,105 @@ export default function SideBar({ children }) {
           </div>
           <div className="flex flex-col justify-between h-full">
             <div className="flex flex-col gap-2 mt-4">
-              <NavLink href="/" icon={homeIcon} activeIcon={homeIconActive} label={t('layout.home')} isActive={isActive} />
-              <NavLink href="/search" icon={searchIcon} activeIcon={searchIconActive} label={t('layout.search')} isActive={isActive} />
-              <NavLink href="/explore" icon={compas} activeIcon={compasActive} label={t('layout.explore')} isActive={isActive} />
-              <NavLink href="/reels" icon={video} activeIcon={videoActive} label={t('layout.reels')} isActive={isActive} />
-              <NavLink href="/chats" icon={message} activeIcon={messageActive} label={t('layout.message')} isActive={isActive} />
-              <NavLink href="/notification" icon={like} activeIcon={likeActive} label={t('layout.notification')} isActive={isActive} />
+              <NavLink
+                href="/"
+                icon={homeIcon}
+                activeIcon={homeIconActive}
+                label={t("layout.home")}
+                isActive={isActive}
+              />
+              <NavLink
+                href="/search"
+                icon={searchIcon}
+                activeIcon={searchIconActive}
+                label={t("layout.search")}
+                isActive={isActive}
+              />
+              <NavLink
+                href="/explore"
+                icon={compas}
+                activeIcon={compasActive}
+                label={t("layout.explore")}
+                isActive={isActive}
+              />
+              <NavLink
+                href="/reels"
+                icon={video}
+                activeIcon={videoActive}
+                label={t("layout.reels")}
+                isActive={isActive}
+              />
+              <NavLink
+                href="/chats"
+                icon={message}
+                activeIcon={messageActive}
+                label={t("layout.message")}
+                isActive={isActive}
+              />
+              <NavLink
+                href="/notification"
+                icon={like}
+                activeIcon={likeActive}
+                label={t("layout.notification")}
+                isActive={isActive}
+              />
 
               <div className="flex items-center gap-4 w-[90%] m-auto rounded-md h-[52px] px-4 hover:bg-gray-100 cursor-pointer">
                 {action}
-                <p className="text-lg">{t('layout.create')}</p>
+                <p className="text-lg">{t("layout.create")}</p>
               </div>
 
-              <div className='flex items-center gap-2 ml-[7%]'>
-                <img src={`${API}/images/${info?.image}`} className='w-10 h-10 rounded-[50%]'  alt="" />
-                <NavLink href="/profile" icon={<Image className={`${pathname === '/profile' ? 'border-2 border-black rounded-full' : ''} h-10 w-10`} src={Profile} alt="Profile" />} label={t('layout.profile')} isActive={isActive} />
+              <div className="flex items-center gap-2 ml-[7%]">
+                <img
+                  src={
+                    info?.image ? `${API}/images/${info.image}` : Profile.src
+                  }
+                  className="w-10 h-10 rounded-full"
+                  alt="Profile"
+                />
+
+                <NavLink
+                  href="/profile"
+                  icon={
+                    <Image
+                      className={`h-10 w-10 ${
+                        pathname === "/profile"
+                          ? "border-2 border-black rounded-full"
+                          : ""
+                      }`}
+                      src={Profile}
+                      alt="Profile"
+                    />
+                  }
+                  label={t("layout.profile")}
+                  isActive={isActive}
+                />
               </div>
             </div>
 
             <div className="flex items-center gap-4 w-[90%] m-auto rounded-md h-[52px] px-4 hover:bg-gray-100">
               {threads}
-              <p className="text-lg">{t('layout.threads')}qdw</p>
+              <p className="text-lg">{t("layout.threads")}qdw</p>
             </div>
 
             <div className="flex items-center gap-4 w-[90%] m-auto rounded-md h-[52px] px-4 hover:bg-gray-100">
               <button onClick={handleClick} className="flex gap-5">
                 {setting}
-                <p className="text-lg">{t('layout.more')}</p>
+                <p className="text-lg">{t("layout.more")}</p>
               </button>
               <Menu
                 anchorEl={anchorEl}
                 open={open}
                 onClose={handleClose}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-              >
-               
-              </Menu>
+                anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+                transformOrigin={{ vertical: "top", horizontal: "center" }}
+              ></Menu>
             </div>
           </div>
         </div>
       </section>
 
-      <div className="ml-[320px]">
-        {children}
-      </div>
+      <div className="ml-[320px]">{children}</div>
     </div>
   );
 }
