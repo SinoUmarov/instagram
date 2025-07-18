3b86cc6aa502705fd97a07b437cec0078f7fe931
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2025-07-18 15:38:21 +0300

    finish the search features

--- src/components/layout/mini-side-bar/mini-side-bar.jsx
+++ src/components/layout/mini-side-bar/mini-side-bar.jsx
@@ -7,6 +7,7 @@ import { useTranslation } from "react-i18next"
 import styled from "@emotion/styled"
 import Image from "next/image"
 import { usePathname, useRouter } from "next/navigation"
+import InstagramIcon from '@mui/icons-material/Instagram'
 
 import {
   add,
@@ -48,7 +49,8 @@ const LightTooltip = styled(({ className, ...props }) => (
 
 const MiniSideBar = ({ children }) => {
 
-  const { toggleDrawer, openDrawer } = useDrawerStore()
+
+  const { toggleDrawer, openDrawer, isOpen } = useDrawerStore()
   const { getInfo, info } = useProfileStore()
   const router = useRouter()
   const pathname = usePathname()
@@ -91,11 +93,12 @@ const MiniSideBar = ({ children }) => {
   return (
     <div className="flex">
       {!isAuthPage && (
-        <section className="flex justify-center w-[50px] border-r-[2px] border-[#eee] h-[100vh]">
+        <section className="flex justify-center w-[50px] border-r-[2px] border-[#eee] h-[100vh]" style={{ marginRight: isOpen == true && pathname !== '/chats' ? '325px' : '0px' }} >
           <div className="sideBar h-full pb-[100px]">
-            {/* <div className="m-auto flex justify-center pb-[10px] mt-[20px]">
-              {instagramMiniLogo}
-            </div> */}
+            <div className="m-auto flex justify-center pb-[10px] mt-[20px]">
+              {/* {instagramMiniLogo} */}
+              <InstagramIcon />
+            </div>
             <div className="flex flex-col justify-between h-full">
               <div className="flex flex-col gap-[0.5rem] mt-4">
                 {/* Home Icon */}
@@ -196,60 +199,3 @@ const MiniSideBar = ({ children }) => {
 
 export default MiniSideBar
 
-// <Menu
-// id="fade-button"
-// sx={{
-// 	padding: 0,
-// 	borderRadius: '10px',
-// 	'.MuiPaper-root': {
-// 		borderRadius: '16px',
-// 		width: '300px',
-// 		height: '475px',
-// 		boxShadow: '0 4px 12px rgba(0, 0, 0, 0.15)',
-// 	},
-// }}
-// anchorEl={anchorEl}
-// open={open}
-// onClose={handleClose}
-// anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-// transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-// >
-// <div className="pb-[10px] bg-[#fff] dark:text-white dark:bg-[#262626] rounded-[16px] shadow-lg p-[10px] border-[1px] w-[300px] h-[475px]">
-// 	<div className="flex flex-col gap-[7px]">
-// 		<Link href="/setting" passHref>
-// 			<MenuItem
-// 				sx={{
-// 					padding: '16px',
-// 					display: 'flex',
-// 					gap: '10px',
-// 					borderRadius: '8px',
-// 				}}
-// 			>
-// 				{setting}
-// 				<p>{t('layout.mores.setting')}</p>
-// 			</MenuItem>
-// 		</Link>
-// 		{/* Add more MenuItems here */}
-// 	</div>
-// </div>
-// </Menu>
-{
-  /* Threads and More */
-}
-
-{
-  /* <div className="flex flex-col items-center super-svg gap-4 w-[90%] rounded-[8px] h-[52px] px-0 justify-center">
-<LightTooltip title={t('layout.threads')} placement="right" arrow>
-  <button onClick={handleClick} className="flex gap-5">
-    {threads}
-  </button>
-</LightTooltip>
-<LightTooltip title={t('layout.more')} placement="right" arrow>
-  <button onClick={handleClick} className="flex gap-5">
-    {settings}
-  </button>
-</LightTooltip>
-
-
-</div> */
-}
